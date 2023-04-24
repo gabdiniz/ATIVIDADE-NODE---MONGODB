86b6c2072cffff480f308635a7b228ceeaf095df
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-24 14:40:27 -0300

    Criação projeto

---
+++ index.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL);
+
+app.listen(3000, () => console.log("Servidor rodando em http://localhost:3000/"));
