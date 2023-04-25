e6e7ef340599dbe706efdba7b6dd7ecdbfdf0a58
Gabriel Diniz <gabrieldiniz.contato@gmail.com>
2023-04-25 09:43:12 -0300

    Aplicando Query

--- routes/produtos.js
+++ routes/produtos.js
@@ -5,13 +5,28 @@ const uploadImage = require("../middlewares/uploadImage");
 const router = Router();
 
 router.get("/produtos", async (req, res) => {
+
+  const { nome, categoria, preco, } = req.query;
+  if ( nome ) {
+    const produtos = await Produto.find({ nome: nome }).exec();
+    return (produtos) ? res.status(200).json(produtos) : res.status(404).json({ message: "Nenhum produto encontrado." });
+  }
+  if ( categoria  ) {
+    const produtos = await Produto.find({ categoria: categoria }).exec();
+    return (produtos) ? res.status(200).json(produtos) : res.status(404).json({ message: "Nenhum produto encontrado." });
+  }
+  if ( preco ) {
+    const produtos = await Produto.find({ preco: preco }).exec();
+    return (produtos) ? res.status(200).json(produtos) : res.status(404).json({ message: "Nenhum produto encontrado." });
+  }
+
   try {
     const produtos = await Produto.find({});
     if (produtos) {
       res.status(200).json(produtos);
     }
     else {
-      res.status(200).json({ message: "Nenhum produto encontrado." });
+      res.status(404).json({ message: "Nenhum produto encontrado." });
     }
   }
   catch (e) {
@@ -57,9 +72,10 @@ router.post("/produtos", uploadImage.single('imagemProduto'), async (req, res) =
   }
 });
 
-router.put("/produtos/:id", async (req, res) => {
+router.put("/produtos/:id", uploadImage.single('imagemProduto'), async (req, res) => {
   try {
-    const { nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imagemProduto } = req.body;
+    const { nome, descricao, quantidade, preco, desconto, dataDesconto, categoria } = req.body;
+    const imagemProduto = req.file.path;
     const { error, value } = schema.validate({ nome, descricao, quantidade, preco, desconto, dataDesconto, categoria, imagemProduto })
     if (error) {
       res.status(400).json({ error: error.message });
